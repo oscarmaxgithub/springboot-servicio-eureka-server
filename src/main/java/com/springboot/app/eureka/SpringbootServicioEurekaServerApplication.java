50598271cbfbfae02b3271319ea83803bc516e8d
oscar fernando diaz anchay <diazanchayoscarfernando@gmail.com>
2021-01-21 22:19:59 -0500

    solucionado error eurka server sin login, e implementacion de balanceo de carga con eurka Server

--- src/main/java/com/springboot/app/eureka/SpringbootServicioEurekaServerApplication.java
+++ src/main/java/com/springboot/app/eureka/SpringbootServicioEurekaServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
 @SpringBootApplication
+//@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableEurekaServer
 public class SpringbootServicioEurekaServerApplication {
 
